f96c3cae9ae7199f1cccc01c60fd809e13ab02b0
fir0z <firoj.is.available@gmail.com>
2018-08-02 15:28:59 +0530

    Adding interface UI

---
+++ ftpClientServer.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo -e "CLIENT-SERVER DEMONSTRATION USING FTP CONNECTION \n-------------------------------------------------"
+
+echo "1. Show file in the Current Directory"
+echo "2. Transfer the file to the Server"
+echo "3. Get the file from the server"
